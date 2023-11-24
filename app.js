0b8b08a845464dd7631a3f4aabb7d48d597fc358
Naman Jha <jhanaman786@gmail.com>
2023-11-24 15:35:42 +0530

    Add CSS

--- app.js
+++ app.js
@@ -2,28 +2,31 @@ const express = require("express");
 const app = express();
 
 app.use(express.urlencoded({extended:true}));
+app.use(express.static("public"));
 
 app.set("view engine", "ejs");
 
-var items = [];
+let items = [];
 
 app.get("/", (req, res)=> {
-    var today = new Date();
-    var options = {
+    let today = new Date();
+    let options = {
         weekday: 'long',
         month: 'long',
         day: 'numeric'
     }
-    var day = today.toLocaleDateString("en-US", options)
-    res.render("list", {
-        kindOfDay: day, listItems: items
-    });
+    let day = today.toLocaleDateString("en-US", options)
+    res.render("list", { listTitle: day, listItems: items });
 });
 
 app.post("/", (req, res)=>{
-    items.push(req.body.addItem);
-    res.redirect("/")
+        items.push(req.body.addItem);
+        res.redirect("/");
 });
+
+app.get("/about", (req, res)=>{
+    res.render("about");
+})
 
 app.listen(3000, function () {
     console.log("Server started at port 3000.")

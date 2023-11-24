53db238ce86b3d4e815391bba9e00be2281ed692
Naman Jha <jhanaman786@gmail.com>
2023-11-24 08:25:41 +0530

    Add ability to add item to the list

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
 const express = require("express");
 const app = express();
 
-app.get("/", function(req, res){
-    res.send("Hello World!");
+app.use(express.urlencoded({extended:true}));
+
+app.set("view engine", "ejs");
+
+var items = [];
+
+app.get("/", (req, res)=> {
+    var today = new Date();
+    var options = {
+        weekday: 'long',
+        month: 'long',
+        day: 'numeric'
+    }
+    var day = today.toLocaleDateString("en-US", options)
+    res.render("list", {
+        kindOfDay: day, listItems: items
+    });
 });
 
-app.listen(3000, function(){
+app.post("/", (req, res)=>{
+    items.push(req.body.addItem);
+    res.redirect("/")
+});
+
+app.listen(3000, function () {
     console.log("Server started at port 3000.")
 });
